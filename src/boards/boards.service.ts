c7fb5d5d01f2d0a657fcaa811908c7ff62a908c7
greeng00se <thegreengoose7@gmail.com>
2022-06-14 08:04:01 +0900

    모델 생성 및 게시물 생성 서비스 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-    private boards = ['name'];
+    private boards: Board[] = [];
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
