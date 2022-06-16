23f415c7cda1f300a0484f78f87c3c162b35b9b2
greeng00se <thegreengoose7@gmail.com>
2022-06-16 14:09:08 +0900

    Get?

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
 
-    
+    @Get('/:id')
+    getBoardById(@Param() id:number) : Promise<Board> {
+        return this.boardService.getBoardById(id);
+    }
 }
