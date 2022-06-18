1bf5c20144f0a3b5996d6ed8115d9d187d91e2dc
greeng00se <thegreengoose7@gmail.com>
2022-06-18 22:48:16 +0900

    게시물 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
 
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardService.createBoard(createBoardDto);
+    }
+
     @Get('/:id')
-    getBoardById(@Param() id:number) : Promise<Board> {
+    getBoardById(@Param() id: number) : Promise<Board> {
         return this.boardService.getBoardById(id);
     }
 }
