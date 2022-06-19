278b69500e132d6e14353683c082f5a6b7ca6fb8
greeng00se <thegreengoose7@gmail.com>
2022-06-19 11:26:41 +0900

    password 암호화, JWT
    - bcrypt
    - JwtModule

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 
@@ -8,9 +10,25 @@ export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
+        private jwtService: JwtService
     ) {}
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return await this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOne({ username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // 유저 토큰 생성
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return { accessToken: accessToken };
+        } else {
+            throw new UnauthorizedException('login failed');
+        }
     }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,9 @@
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,7 +11,9 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         
-        const user = this.create({ username, password });
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword });
         
         try {
             await this.save(user);
